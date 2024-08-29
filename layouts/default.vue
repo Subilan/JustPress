cc11f5177922f238077dda886cb177deb5c60085
Subilan <suyuanliang1995@gmail.com>
2024-08-29 18:50:05 +0800

    feat: search button

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,10 @@
     <span class="site-title">
       <router-link to="/">Subilan's Blog</router-link>
     </span>
+    <div class="search-btn">
+      <icon :path="mdiMagnify"/>
+      搜索<span class="hotkey">{{ isMacOS() ? '⌘' : 'Ctrl'}}+K</span>
+    </div>
     <div class="spacer"/>
     <div class="nav-links">
       <div class="nav-link" v-for="x in pages">
@@ -18,6 +22,11 @@
 
 <script setup>
 import {pages} from "~/data/config.js";
+import {mdiMagnify} from "@mdi/js";
+
+function isMacOS() {
+  return navigator.userAgent.includes('Macintosh');
+}
 </script>
 
 <style lang="scss">
@@ -69,6 +78,36 @@ import {pages} from "~/data/config.js";
 
   .nav-link-inner.router-link-exact-active {
     border-bottom: 2px solid #009688;
+  }
+}
+
+.search-btn {
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  padding: 4px 6px;
+  border: 1px solid rgba(0, 0, 0, .1);
+  transition: all .2s ease;
+  line-height: 1;
+  cursor: pointer;
+  border-radius: 5px;
+  gap: 4px;
+  margin-left: 16px;
+
+  svg {
+    height: 16px;
+    width: 16px;
+  }
+
+  .hotkey {
+    font-size: 12px;
+    color: #aaa;
+  }
+
+  &:hover {
+    transform: translateY(-2px);
+    box-shadow: 0 4px 0 rgba(0, 0, 0, .1);
+    border: 1px solid rgba(#004d40, .8);
   }
 }
 </style>
