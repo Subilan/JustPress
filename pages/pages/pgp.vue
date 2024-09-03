b39a6ddbfefda4917d3f7ecbb5a0b8f1acb940b8
Subilan <suyuanliang1995@gmail.com>
2024-09-03 19:52:03 +0800

    chore: pgp page

---
+++ pages/pages/pgp.vue
@@ -0,0 +1,17 @@
+<template>
+  <article>
+    <h1>PGP</h1>
+    <ul>
+      <li>Fingerprint — BC66A8EAA950B9823A309871FC61A89A8F418BD5</li>
+      <li>Valid Until — 2027/01/01</li>
+      <li>User ID — Subilan &lt;<a href="mailto:christophersubilan@gmail.com">christophersubilan@gmail.com</a>&gt;</li>
+    </ul>
+    <pre class="language-">
+      <code v-text="pgp"/>
+    </pre>
+  </article>
+</template>
+
+<script setup>
+import pgp from '~/data/pgp.js'
+</script>
