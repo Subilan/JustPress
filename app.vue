37e5f1a98507c0490bef901b54a0489a998e7b3a
Subilan <suyuanliang1995@gmail.com>
2024-09-03 21:18:08 +0800

    chore: add nuxt loading indicator

--- app.vue
+++ app.vue
@@ -1,8 +1,9 @@
 <template>
+  <nuxt-loading-indicator color="#1de9b6" :throttle="0"/>
   <nuxt-layout>
     <nuxt-page/>
   </nuxt-layout>
 </template>
 
-<script setup lang="ts">
+<script setup>
 </script>
